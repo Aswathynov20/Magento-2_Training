8fe950fd05e2290e1afc1f3694f2683ae3c9df50
Aswathy Devi M L <aswathy.devi@eglobeits.com>
2023-12-04 12:05:48 +0530

    second coomit

---
+++ app/etc/env.php
@@ -0,0 +1,85 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'cache' => [
+        'graphql' => [
+            'id_salt' => 'TRi8nir9MS10Jc3bMtCVGT0OB2eNIB9s'
+        ],
+        'frontend' => [
+            'default' => [
+                'id_prefix' => 'd42_'
+            ],
+            'page_cache' => [
+                'id_prefix' => 'd42_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => '6386069e6c77936cbdb489f62f05a19d'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'magento2',
+                'username' => 'root',
+                'password' => 'Admin@123',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'lock' => [
+        'provider' => 'db'
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'downloadable_domains' => [
+        'magentotraining.com'
+    ],
+    'install' => [
+        'date' => 'Sun, 03 Dec 2023 13:18:04 +0000'
+    ]
+];
---
+++ pub/static/frontend/Magento/luma/en_US/jquery/ui-modules/effects/effect-blind.js
@@ -0,0 +1,1 @@
+/home/aswathyd/Documents/projects/magento2/lib/web/jquery/ui-modules/effects/effect-blind.js
